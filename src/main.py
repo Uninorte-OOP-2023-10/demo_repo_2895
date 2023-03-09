0d915909e608b3e032a0e395815426a50f703375
unknown <Administrador@SU301-100972-L.CampusUninorte.local>
2023-03-09 09:24:38 -0500

    Added some files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
+#from user.person import *
+
+from prueba.prueba import Test
+
+"""
 def main():
     nombre = input('Digite su nombre: ')
-    print('Su nombre es', nombre)
+    edad = int(input('Digite su edad: '))
+    person1 = Person(nombre, edad)
+    print(person1)
 
+    nombre = input('Digite el nombre: ')
+    edad = int(input('Digite la edad: '))
+    telefono = int(input('Digite el telefono: '))
+    friend1 = Friend(nombre, edad, telefono)
+    print(friend1)
+"""
+
+def main():
+    nombre = input('Digite su nombre: ')
+    edad = int(input('Digite su edad: '))
+    test1 = Test(nombre, edad)
+
+    print(test1.person)
+    
 
 if __name__ == '__main__':
     main()
